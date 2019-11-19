45d25145e05b5687fe96ae1e66911110b67a15bf
Sacek073 <janeksacek@seznam.cz>
2019-11-19 07:54:12 +0100

    simple table print

--- test_main.c
+++ test_main.c
@@ -6,25 +6,22 @@
 #include "parser.c"
 
 hSymtab *table;
-/*
+
 void print_sym_tab(hSymtab *table){
   DEBUG_PRINT("PRINTING WHOLE TABLE\n\n");
   for (int i = 0; i < HTAB_PRIME; i++){
-    if ((*table)[i] != NULL) {
-      if ((*table)[i]->data->type == TypeInt)
-        printf("\t |Variable: %s| \t |Value: %d| \t |Type: INT|\n", (*table)[i]->hKey, (*table)[i]->data->value_int);
-      else if ((*table)[i]->data->type == TypeString)
-        printf("\t |Variable: %s| \t |Value: %s| \t |Type: STRING|\n", (*table)[i]->hKey, (*table)[i]->data->value_str);
-      else if ((*table)[i]->data->type == TypeFloat)
-        printf("\t |Variable: %s| \t |Value: %f| \t |Type: FLOAT|\n", (*table)[i]->hKey, (*table)[i]->data->value_float);
+    if ((*table)[i]) {
+      printf("%s %d\n", (*table)[i]->hKey, (*table)[i]->item_type);
     }
   }
 }
-*/
+
 
 int main() {
   prog();
-  //print_sym_tab(table);
-  //printf("\n");
-  //DEBUG_PRINT("_____PARSING ENDED_____\n");
+
+  print_sym_tab(table);
+  printf("\n");
+  DEBUG_PRINT("_____PARSING ENDED_____\n");
+
 }

d4c66b945103526eda2bd503039d86c618debc11
Boris <xburka00@vutbr.cz>
2019-11-19 20:41:38 +0100

    New:     -type checking and error checking in parser.c

--- parser.c
+++ parser.c
@@ -61,41 +61,80 @@ extern char current_line[1000];
 int err = 0;
 extern hSymtab *table = NULL;
 
+
 void assignment(Token *var, Token *value){
   if (GET_TOKEN_CHECK_EOF(value) || TOKEN_TYPE_NEEDED_CHECK(value->type, TypeNewLine)) {DEBUG_PRINT("Reached EOF or Newline where it shouldn't be\n"); exit(1);}
 
+  hSymtab_it *tmp_item_var;
+  hSymtab_it *tmp_item_value;
   // TODO if the variable has already been added to symtab
-
-  hSymtab_it *tmp_it;
-  switch(value->type){
-    // Add variable as Integer
-    case TypeInt:
-      symtab_add_it(table, var);
-      symtab_add_var_data((*table)[symtab_hash_function((char*)var->data)], TypeInt);
-      break;
-    // Add variable as String
-    case TypeString:
-      symtab_add_it(table, var);
-      symtab_add_var_data((*table)[symtab_hash_function((char*)var->data)], TypeString);
-      break;
-    // Add variable as Float
-    case TypeFloat:
-      symtab_add_it(table, var);
-      symtab_add_var_data((*table)[symtab_hash_function((char*)var->data)], TypeFloat);
-      break;
-    // Add variable as same type as Variable
-    case TypeVariable:
-      if (!(tmp_it = symtab_it_position((char*)value->data, table))) {
-        DEBUG_PRINT("SYNTAX ERROR: The variable %s is non-existent.\n", (char*)value->data);
-        exit(1);
+  if ((tmp_item_var = symtab_it_position((char*)var->data, table))) {
+    if (!(tmp_item_value = symtab_it_position((char*)value->data, table))) {
+      if (tmp_item_var->item_type == IT_VAR) {
+        if (!(TOKEN_TYPE_NEEDED_CHECK(((hSymtab_Var*)tmp_item_var->data)->type, value->type))) {
+          DEBUG_PRINT("SYNTAX ERROR: Variable assigning incorrect value to variable %s.\n", (char*)var->data);
+          exit(1);
+        }
       }
-      symtab_add_it(table, var);
-      symtab_add_var_data((*table)[symtab_hash_function((char*)var->data)], ((hSymtab_Var*)(tmp_it->data))->type);
-      break;
+    }
+    else {
+      if (tmp_item_value->item_type == IT_VAR) {
+        if (!(TOKEN_TYPE_NEEDED_CHECK(((hSymtab_Var*)tmp_item_var->data)->type, ((hSymtab_Var*)tmp_item_value->data)->type))) {
+          DEBUG_PRINT("SYNTAX ERROR: Variable assigning incorrect value to variable %s.\n", (char*)var->data);
+          exit(1);
+        }
+      }
+      else if (tmp_item_value->item_type == IT_FUNC){
+        if (!(TOKEN_TYPE_NEEDED_CHECK(((hSymtab_Var*)tmp_item_var->data)->type, ((hSymtab_Func*)tmp_item_value->data)->return_type))) {
+          DEBUG_PRINT("SYNTAX ERROR: Variable assigning incorrect value to variable %s.\n", (char*)var->data);
+          exit(1);
+        }
+      }
+    }
+  }
+  else {
+    hSymtab_it *tmp_it;
+    switch(value->type){
+      // Add variable as Integer
+      case TypeInt:
+        symtab_add_it(table, var);
+        symtab_add_var_data((*table)[symtab_hash_function((char*)var->data)], TypeInt);
+        break;
+      // Add variable as String
+      case TypeString:
+        symtab_add_it(table, var);
+        symtab_add_var_data((*table)[symtab_hash_function((char*)var->data)], TypeString);
+        break;
+      // Add variable as Float
+      case TypeFloat:
+        symtab_add_it(table, var);
+        symtab_add_var_data((*table)[symtab_hash_function((char*)var->data)], TypeFloat);
+        break;
+      // Add variable as same type as Variable
+      case TypeVariable:
+        if (!(tmp_it = symtab_it_position((char*)value->data, table))) {
+          DEBUG_PRINT("SYNTAX ERROR: The variable %s is non-existent.\n", (char*)value->data);
+          exit(1);
+        }
+        if (tmp_it->item_type == IT_VAR) {
+          symtab_add_it(table, var);
+          symtab_add_var_data((*table)[symtab_hash_function((char*)var->data)], ((hSymtab_Var*)(tmp_it->data))->type);
+        }
+        else if (tmp_it->item_type == IT_FUNC) {
+          symtab_add_it(table, var);
+          symtab_add_var_data((*table)[symtab_hash_function((char*)var->data)], ((hSymtab_Func*)(tmp_it->data))->return_type);
+        }
+        else {
+          DEBUG_PRINT("UNDEFINED ERROR.\n");
+          exit(1);
+        }
 
-    default:
-      break;
+        break;
 
+      default:
+        break;
+
+    }
   }
 }
 
@@ -127,9 +166,10 @@ void body(Token *token){
         command(token);
       }
       else{
-        DEBUG_PRINT("Found EOF, parsing terminated.\n");
+        DEBUG_PRINT("Found EOF, parsing terminated.  %s\n", (char *)token->data);
         return;
       }
+      if (GET_TOKEN_CHECK_EOF(token)) {DEBUG_PRINT("Found EOF, parsing terminated.\n"); return;}
 
   }
 }
@@ -168,9 +208,14 @@ int fction_params(Token *token, hSymtab_it *symtab_it){
         params = params->next;
       }
 
+      //zapsat parametry do tabulky bez typu
+      /*
+      hSymtab_Func_Param *func_param;
+      *func_param = (hSymtab_Func_Param) malloc(sizeof(fct_param));
+      */
     }
     else if(TOKEN_TYPE_NEEDED_CHECK(token->type, TypeComma)){
-      //printf("comma\n");
+      printf("comma\n");
       check_comma = 1;
     }
     else{

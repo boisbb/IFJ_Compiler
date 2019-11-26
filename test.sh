66da17f27c22d32588b6bf1600c0a869bb787658
Sacek073 <janeksacek@seznam.cz>
2019-11-26 21:03:55 +0100

    test script that should work

--- test.sh
+++ test.sh
@@ -5,12 +5,48 @@ x=0
 if [ "$1" == "-h" ]; then
   x=1
   echo
-  echo "Jako první argument zadej překládaný kód, jako druhý agrument zadej očekávaný výstup"
+  echo "první argument = soubor, který se bude překládat, optional - druhý = vstupní soubor (.in)"
   echo
-  echo "např.: ./test.sh in1.txt out1.txt"
+  echo "např.: ./test.sh program.ifj vstupni.in"
   echo
   exit
 fi
+
+if [ "$#" == 2 ]; then
+  cat ./ifj19.py "$1" > testPrg.py
+  python3 testPrg.py < "$2" > test.out #výsledek pythonu
+
+  #./ifj_compiler < "$1" >final.out
+
+  ./ic19int final.out > result #vstupní souboru asi nějak předat
+
+  echo "start diffu -->"
+  #diff result test.out
+  echo "<-- konec diffu"
+
+  #rm final.out test.out result
+
+  exit
+fi
+
+if [ "$#" == 1 ]; then
+  cat ./ifj19.py "$1" > testPrg.py
+  python3 testPrg.py > test.out #výsledek pythonu
+
+  #./ifj_compiler < "$1" >final.out
+
+  ./ic19int final.out > result
+
+
+  echo "start diffu -->"
+  #diff result test.out
+  echo "<-- konec diffu"
+
+  #rm final.out test.out result
+
+  exit
+fi
+
 
 if [ "$#" != 2 ]; then
   if [ "$x" == 0 ]; then
@@ -21,13 +57,4 @@ if [ "$#" != 2 ]; then
   fi
 fi
 
-echo "start diffu -->"
-
-cat "$1" | ./a.out > vystup.tmp
-
-diff vystup.tmp "$2"
-
-rm vystup.tmp
-
-echo "<-- konec diffu"
 exit
